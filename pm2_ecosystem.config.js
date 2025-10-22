4ff9e60e8638190d1e1f5f75fcefc59f7a6ef2c4
vuvanhoang <hoangvuvan677@gmail.com>
2025-10-22 22:06:47 +0700

    update: bo sung script chay

--- pm2_ecosystem.config.js
+++ pm2_ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
 
     {
-      name: "springboot-gold-service",
+      name: "springboot-unischedule-service",
       script: "java",
-      args: "-jar target/gold_price_service-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod",
-      cwd: "/Users/vuvanhoang/WorkSpace/project/gold_price/gold_price_service" 
+      args: "-jar target/UniScheduleService-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod",
+      cwd: "/Users/vuvanhoang/WorkSpace/project/unischedule/unischedule_service" 
     }
   ]
 };

f5120e3f9e2c9a52431b4e05d1d45c8ad171faf9
hoangvuvan0611 <hoangvuvan677@gmail.com>
2026-01-14 23:24:15 +0700

    sua lenh compile

--- run_build_with_pm2.sh
+++ run_build_with_pm2.sh
@@ -1,7 +1,7 @@
 echo 'Delete service running in PM2'
 pm2 delete springboot-unischedule-service
 echo 'Compile jar file'
-mnv clean package -DskipTest
+mvn clean package -DskipTest
 echo 'Compile complete'
 echo 'Run Service'
 pm2 start ./pm2_ecosystem.config.js

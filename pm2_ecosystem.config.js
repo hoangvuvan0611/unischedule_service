57eaec474635d3061dd738327c837a95a84e4e1b
hoangvuvan0611 <hoangvuvan677@gmail.com>
2026-01-20 00:19:53 +0700

    update

--- pm2_ecosystem.config.js
+++ pm2_ecosystem.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-    apps: [
-        {
-            name: "springboot-unischedule-service",
-            script: "java",
-            args: "-jar target/UniScheduleService-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod",
-            cwd: "/Users/vuvanhoang/WorkSpace/project/unischedule/unischedule_service"
-        }
-    ]
+  apps: [
+    {
+      name: "springboot-unischedule-service",
+      script: "java",
+      args: [
+        "-jar",
+        "target/UniScheduleService-0.0.1-SNAPSHOT.jar",
+        "--spring.profiles.active=prod"
+      ],
+      cwd: "/Users/vuvanhoang/WorkSpace/project/unischedule/unischedule_service"
+    }
+  ]
 };
